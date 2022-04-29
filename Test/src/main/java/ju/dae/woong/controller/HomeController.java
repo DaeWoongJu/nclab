d6603f75598691c0165a87435fbe1d0476b2ffed
DaeWoongJu <woong6962@naver.com>
2022-04-29 09:49:00 +0900

    contextpath cp

--- Test/src/main/java/ju/dae/woong/controller/HomeController.java
+++ Test/src/main/java/ju/dae/woong/controller/HomeController.java
@@ -1,19 +1,25 @@
 package ju.dae.woong.controller;
 
 
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
 public class HomeController {
-	
+	@Autowired private ServletContext sc;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
-	
-		return "home";
-	}
-	
+	public String home(Model model) {
+		sc.setAttribute("cp", sc.getContextPath());
+		model.addAttribute("main","/WEB-INF/views/home.jsp");
+		return "layout";
+	}	
 }
--- Test/src/main/webapp/WEB-INF/views/layout.jsp
+++ Test/src/main/webapp/WEB-INF/views/layout.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,14 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<div>
+	<h1>N-CLAB</h1>
+</div>
+<div id="headerLoginMenu">
+	<ul>
+		<li><a href="${cp }/">HOME</a></li>
+		<li><a href="${cp}/member/login">login</a></li>
+	</ul>
+</div>
 </body>
 </html>
