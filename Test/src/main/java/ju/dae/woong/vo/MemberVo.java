a3c5ccfb8e262c0262b48bc852880404402fd796
DaeWoongJu <woong6962@naver.com>
2022-04-29 09:20:53 +0900

    vo+ mapper

---
+++ Test/src/main/java/ju/dae/woong/vo/MemberVo.java
@@ -0,0 +1,15 @@
+package ju.dae.woong.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Data
+public class MemberVo {
+	private String writer;
+	private String pwd;
+}
